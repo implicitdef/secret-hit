fc3127af2822800ca69622a1587abafa6301f374
implicitdef <implicitdef@users.noreply.github.com>
2016-08-27 11:45:35 +0200

    added game

--- db/migrate/20160827073729_create_teams.rb
+++ db/migrate/20160827073729_create_teams.rb
@@ -1,7 +1,6 @@
 class CreateTeams < ActiveRecord::Migration[5.0]
   def change
     create_table :teams do |t|
-
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160827074725) do
+ActiveRecord::Schema.define(version: 20160827082712) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "games", force: :cascade do |t|
+    t.integer  "team_id"
+    t.string   "policies_stack",                        array: true
+    t.string   "discarded_stack",                       array: true
+    t.string   "played_policies",                       array: true
+    t.string   "president_choice"
+    t.boolean  "chancellor_asks_for_veto"
+    t.integer  "turns_without_elections"
+    t.string   "current_step"
+    t.datetime "created_at",               null: false
+    t.datetime "updated_at",               null: false
+    t.index ["team_id"], name: "index_games_on_team_id", using: :btree
+  end
 
   create_table "players", force: :cascade do |t|
     t.integer  "team_id"
